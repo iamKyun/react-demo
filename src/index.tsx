f7578a6c55b56784c6c1ace2a8a839f6ecab95d4
Kyun <kyun.c137@gmail.com>
2020-08-07 18:26:29 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,96 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Debugger } from 'inspector'
+
+interface SquareProps {
+  value: boolean
+  onClick: () => void
+}
+
+interface BoardProps {
+  x: number,
+  y: number
+}
+
+interface BoardState {
+  squares: boolean[][],
+  xIsNext: boolean
+}
+
+function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value == null ? '' : props.value ? '⚫' : '⚪'}
+    </button>
+  )
+}
+
+class Board extends React.Component<BoardProps, BoardState> {
+  constructor(props: BoardProps) {
+    super(props)
+    this.state = {
+      squares: function () {
+        const squares: boolean[][] = new Array(props.x)
+        for (let x = 0; x < props.x; x++) {
+          squares[x] = new Array(props.y)
+        }
+        return squares
+      }(),
+      xIsNext: true
+    }
+  }
+
+  renderSquare(x: number, y: number) {
+    return <Square value={this.state.squares[x][y]} onClick={() => this.handleClick(x, y)} />
+  }
+
+  handleClick(x: number, y: number) {
+    const squares = this.state.squares.slice()
+    squares[x][y] = this.state.xIsNext
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
+  }
+
+  render() {
+    const status = 'Next player: ' + (this.state.xIsNext ? '⚫' : '⚪')
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        {
+
+          this.state.squares.forEach((itemX, x) => {
+            return <div className="board-row">{
+              itemX.forEach((itemY, y) =>
+                this.renderSquare(x, y)
+              )
+            })</div>
+          })
+        }
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board x={5} y={5} />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
